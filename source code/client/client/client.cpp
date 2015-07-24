aed48b73766b647e03267b13d53dfae454d3ad1a
MrC2015 <justmukund2012@gmail.com>
2015-07-25 07:37:26 +1200

    updated and added GPLv3
    updated and added GPLv3

--- source code/client/client/client.cpp
+++ source code/client/client/client.cpp
@@ -6,7 +6,24 @@
 #include "stdafx.h"
 #pragma comment(lib, "rpcrt4.lib")
 
+/*liscenced under GPLv3
+ -An Artificial Intelligence Program Called CAST(completely automated self thinking ai).
+    -Copyright (C) 2015  Mukund
 
+    -This program is free software: you can redistribute it and/or modify
+    -it under the terms of the GNU General Public License as published by
+    -the Free Software Foundation, either version 3 of the License, or
+    -(at your option) any later version.
+
+    -This program is distributed in the hope that it will be useful,
+    -but WITHOUT ANY WARRANTY; without even the implied warranty of
+    -MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    -GNU General Public License for more details.
+
+    -You should have received a copy of the GNU General Public License
+    -along with this program.  If not, see http://www.gnu.org/licenses/.
+	-check this folder's root directory for a copy of the program.
+    -you can contact me at justmukund2012@gmail.com or admin@mail.siblingz.net*/
 #include <windows.h> 
 #include <stdio.h>
 #include <conio.h>
